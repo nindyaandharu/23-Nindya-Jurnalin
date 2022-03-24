594c7f53d4a3755a1e800b5848ae3544563b7c1c
nindyaandharu <galuhandharu@gmail.com>
2022-03-24 07:43:19 +0700

    Memperbaiki font

--- jurnalweb/src/App.vue
+++ jurnalweb/src/App.vue
@@ -41,73 +41,230 @@
 
     <!-- Provides the application the proper gutter -->
     
-      <v-img src="./assets/Rectangle 7.png" width="900" height="450" class="mt-10">
-      <v-card-text> 
-        <H3>HAI, NINDYA! SAVE YOUR MEMORIES FOR TODAY!</H3>
-      </v-card-text>
-      </v-img>
+      <!-- <div id="app">
+        <img src="./assets/Rectangle 7.png" alt="Vue Book">
+      </div> -->
+      <v-row>
+      <v-col> 
+        <v-img src="./assets/Rectangle 7.png" width="1000" height="600" class="mt-10" position= "relative"> </v-img>
+      </v-col>
+      <v-col align-self="center">
+        <H3 class="text-hello">
+          HAI, NINDYA! SAVE YOUR MEMORIES FOR TODAY!</H3>
+      </v-col>
+      </v-row>
       <v-card-text class="text-choose mt-5" align="center">choose according to your needs</v-card-text>
 
-    <v-container class="mt-5">
+    <v-container class="mt-5 mb-15">
     <v-row>
-      <v-col
-        cols="12"
-        md="6"
-      >
-      <v-img src="./assets/Ellipse 2.png" width="308" height="302"></v-img>
+      <v-col cols="4"> <v-img src="./assets/Ellipse 2.png"></v-img> </v-col>
+      <v-col cols="6" class="text-diary" align-self="center"> 
+        <h3>DIARY</h3> 
+        <p>You can reminisce all memories to keep them in mind</p>
       </v-col>
-      <v-col
-        cols="12"
-        md="6"
-      >
-      <v-card-text class="text-diary" align="left"> 
-        DIARY
-      </v-card-text>
+      <v-col align-self="center"> 
+        <v-btn
+        outlined
+        color="#66806A"
+        width="120"
+        height="45">
+        CREATE
+        </v-btn>
       </v-col>
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <v-img src="./assets/Ellipse 4.png" width="308" height="302"></v-img>
+    </v-row>
+    <v-row>
+      <v-col cols="4"> <v-img src="./assets/Ellipse 4.png"></v-img> </v-col>
+      <v-col cols="6" class="text-Calender" align-self="center"> 
+        <h3>CALENDER MOOD</h3> 
+        <p>Record details of your mood every day</p>
       </v-col>
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <v-textarea
-          outlined
-          name="input-7-4"
-          label="Outlined textarea"
-          value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-        ></v-textarea>
+      <v-col align-self="center"> 
+        <v-btn
+        outlined
+        color="#66806A"
+        width="120"
+        height="45">
+        CREATE
+        </v-btn>
       </v-col>
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <v-img src="./assets/Ellipse 5.png" width="308" height="302"></v-img>
+    </v-row>
+    <v-row>
+      <v-col cols="4"> <v-img src="./assets/Ellipse 5.png"></v-img> </v-col>
+      <v-col cols="6" class="text-Todo" align-self="center"> 
+        <h3>TO DO</h3> 
+        <p>Write down what you want to do so you don't miss it</p>
       </v-col>
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <v-textarea
-          outlined
-          name="input-7-4"
-          label="Outlined textarea"
-          value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-        ></v-textarea>
+      <v-col align-self="center"> 
+        <v-btn
+        outlined
+        color="#66806A"
+        width="120"
+        height="45">
+        CREATE
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
+
+  <v-img src="./assets/Rectangle 8.png" class="mt-15" height="510">
+  <v-container>
+    <v-card-text class="text-TESTI text-center justify-center white--text">
+      TESTIMONIALS
+    </v-card-text>
+  <v-row>
+    <v-col>
+      <v-card
+        class="mx-auto"
+        color="#E1C892"
+        dark
+        max-width="400">
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://i.pinimg.com/564x/52/4a/ae/524aae7a21f2014b4e6005551f83e704.jpg"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title class="text-h5">Elmeira Navari</v-list-item-title>
+          <v-rating
+          :value="4.5"
+          color="white"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card-actions>
+    <v-card-text class="text-h5 font-weight-bold">
+      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+    </v-card-text>  
+      </v-card>
+    </v-col>
+    <v-col>
+      <v-card
+        class="mx-auto"
+        color="#E1C892"
+        dark
+        max-width="400">
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://i.pinimg.com/564x/c2/cc/9f/c2cc9f200b765e4abec893f54dc9afd3.jpg">
+          </v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title class="text-h5">Erisha Danurdara</v-list-item-title>
+          <v-rating
+          :value="4.5"
+          color="white"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card-actions>
+    <v-card-text class="text-h5 font-weight-bold">
+      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+    </v-card-text>  
+    </v-card>
+    </v-col>
+    <v-col>
+      <v-card
+        class="mx-auto"
+        color="#E1C892"
+        dark
+        max-width="400">
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://i.pinimg.com/564x/56/dc/9f/56dc9f45292a60f48243bbe64df4b6aa.jpg">
+          </v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title class="text-h5">Abel Khaizure</v-list-item-title>
+          <v-rating
+          :value="4.5"
+          color="white"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card-actions>
+    <v-card-text class="text-h5 font-weight-bold">
+      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+    </v-card-text>  
+    </v-card>
+    </v-col>
+  </v-row>
+
+  <center>
+    <v-card-text class="text-RATE white--text mt-10">
+      TAP TO RATE
+    </v-card-text>
+  
+    <v-rating
+      v-model="rating"
+      background-color="white lighten-3"
+      color="white"
+      large
+    ></v-rating>
+  </center>
+  </v-container>
+  </v-img>
       <!-- If using vue-router -->
       <router-view></router-view>
-    
   </v-main>
 
-  <v-footer app>
-    footer
-    <!-- -->
+  <v-footer
+    dark
+    padless
+  >
+    <v-card
+      flat
+      tile
+      class="white--text text-center"
+      color="#E1C892"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+         
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>JURNALIN</strong>
+      </v-card-text>
+    </v-card>
   </v-footer>
 </v-app>
 </template>
@@ -125,15 +282,40 @@ export default {
 };
 </script>
 
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Asap:ital,wght@1,500&display=swap');
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Asap&display=swap');
 .text-choose{
   font-family: 'Asap';
-  font-size: 24px;
+  font-size: 30px !important;
+  font-style: italic;
 }
-@import url('https://fonts.googleapis.com/css2?family=Asap&display=swap');
+.text-hello{
+  font-family: 'Asap';
+  font-size: 70px;
+  line-height: 80px;
+  font-weight: 600;
+  width: 370px;
+}
 .text-diary{
   font-family: 'Asap';
   font-size: 36px;
+}
+.text-Calender{
+  font-family: 'Asap';
+  font-size: 36px;
+}
+.text-Todo{
+  font-family: 'Asap';
+  font-size: 36px;
+}
+.text-TESTI{
+  font-family: 'Asap';
+  font-size: 24px !important;
+  font-style: italic;
+}
+.text-RATE{
+  font-family: 'Asap';
+  font-size: 24px !important;
+  font-style: italic;
 }
 </style>
